a31108091b3d0e73b73cdc9d3eab4ac6a58db530
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2012-04-27 11:00:39 -0700

    Initial commit

---
+++ SlowCheetah.Samples/FSharpDemo/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.net
+let settingOne = System.Configuration.ConfigurationManager.AppSettings.["settingOne"];
+let settingTwo = System.Configuration.ConfigurationManager.AppSettings.["settingTwo"];
+let settingthree = System.Configuration.ConfigurationManager.AppSettings.["settingThree"];
+
+
+printfn "settingOne: %s" settingOne
+printfn "settingTwo: %s" settingTwo
+printfn "settingThree: %s" settingthree
+
+printfn " "
+printfn "Press any key to close"
+System.Console.ReadKey(true)
---
+++ SlowCheetah.Samples/Wpf.Transform/TransformModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Wpf.Transform {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Configuration;
+    using System.IO;
+
+    public class TransformModel {
+        public TransformModel() {
+            this.AppSettings = new Dictionary<string, string>();
+            this.ConnectionStrings = new Dictionary<string, string>();
+        }
+
+        public IDictionary<string, string> AppSettings { get; set; }
+        public IDictionary<string, string> ConnectionStrings { get; set; }
+        public string ConfigContents { get; set; }
+
+        public static TransformModel BuildFromCurrent() {
+            TransformModel tm = new TransformModel();
+            foreach (string key in ConfigurationManager.AppSettings.AllKeys) {
+                tm.AppSettings.Add(key, ConfigurationManager.AppSettings[key]);
+            }
+
+            foreach (ConnectionStringSettings cn in ConfigurationManager.ConnectionStrings) {
+                tm.ConnectionStrings.Add(cn.Name, cn.ConnectionString);
+            }
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            tm.ConfigContents = File.ReadAllText(config.FilePath);
+
+            return tm;
+        }
+    }
+}
