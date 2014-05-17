1959405da8f1c2eedc05b5f7dee61c26d61e3b4f
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-05-17 16:29:52 -0700

    updating version to 2.5.10.3. Minor build update. Related to #140.

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /p:Configuration=Release /p:VisualStudioVersion=11.0 /flp1:v=d;logfile=msbuild.d.log /flp2:v=diag;logfile=msbuild.diag.log
+"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" build.proj /p:Configuration=Release /p:VisualStudioVersion=12.0 /flp1:v=d;logfile=msbuild.d.log /flp2:v=diag;logfile=msbuild.diag.log
 
