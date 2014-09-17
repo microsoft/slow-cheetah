7681c20d9c1796b3628702b3e1288cc6c47884d1
Cris Barbero <cfbarbero@gmail.com>
2014-09-16 22:25:35 -0600

    Add IntegrationTests project and sample project to target for tests.

---
+++ SlowCheetah.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Build.Evaluation;
+
+namespace SlowCheetah.IntegrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BuildConsoleApp()
+        {
+            var p = new Project(@"E:\Project\GitHub\cfbarbero\slow-cheetah\FunctionalTests\ConsoleApp\ConsoleApp.csproj");
+            p.Build();
+        }
+    }
+}
