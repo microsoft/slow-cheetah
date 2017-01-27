5d6791698b3848cfc79cd94c2371b36b70325b6b
Davi Paulino <davilimap@gmail.com>
2017-01-26 17:47:59 -0800

    Updating AssemblyInfo and Appveyor settings

--- SlowCheetah/Properties/AssemblyInfo.cs
+++ SlowCheetah/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SlowCheetah")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Build Transforms")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SlowCheetah")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Sayed Ibrahim Hashimi, Chuck England 2011. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.10728.0")]
+[assembly: AssemblyFileVersion("1.0.10728.0")]
