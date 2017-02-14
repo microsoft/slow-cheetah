23eff5ebe47120292b99b7b063e4bc40362daefd
Davi Paulino <davilimap@gmail.com>
2017-02-13 16:31:49 -0800

    Using Theory to simplify tests

--- SlowCheetah.VisualStudio.Tests/PackageUtilitiesTest.cs
+++ SlowCheetah.VisualStudio.Tests/PackageUtilitiesTest.cs
@@ -12,68 +12,81 @@ namespace SlowCheetah.VisualStudio.Tests
     /// </summary>
     public class PackageUtilitiesTest
     {
-        IEnumerable<string> baseTestProjectConfigs = new List<string>(new string[] { "Debug", "Release"});
+        IEnumerable<string> baseTestProjectConfigs = new List<string>(new string[] { "Debug", "Release" });
+        IEnumerable<string> testProjectConfigsWithDots = new List<string>(new string[] { "Debug", "Debug.Test", "Release", "Test.Release", "Test.Rel" });
+
 
         /// <summary>
         /// Tests <see cref="PackageUtilities.IsFileTransform(string, string, IEnumerable{string})"/> returns on arguments that are null or empty strings
         /// </summary>
-        [Fact]
-        public void IsFileTransfromWithNullArguments()
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData("App.config", null)]
+        [InlineData("App.config", "")]
+        [InlineData(null, "App.Debug.config")]
+        [InlineData("", "App.Debug.config")]
+        public void IsFileTransfromWithNullArguments(string docName, string trnName)
         {
-            Assert.False(PackageUtilities.IsFileTransform(null, null, baseTestProjectConfigs));
-            Assert.False(PackageUtilities.IsFileTransform("", "", baseTestProjectConfigs));
-            Assert.False(PackageUtilities.IsFileTransform("App.config", null, baseTestProjectConfigs));
-            Assert.False(PackageUtilities.IsFileTransform("App.config", "", baseTestProjectConfigs));
-            Assert.False(PackageUtilities.IsFileTransform(null, "App.Debug.config", baseTestProjectConfigs));
-            Assert.False(PackageUtilities.IsFileTransform("", "App.Debug.config", baseTestProjectConfigs));
+            Assert.False(PackageUtilities.IsFileTransform(docName, trnName, baseTestProjectConfigs));
         }
 
         /// <summary>
         /// Tests <see cref="PackageUtilities.IsFileTransform(string, string, IEnumerable{string})"/> with valid arguments normally found in projects.
         /// </summary>
-        [Fact]
-        public void IsFileTransfromWithValidArguments()
+        [Theory]
+        [InlineData("App.config", "App.Debug.config")]
+        [InlineData("App.config", "app.release.config")]
+        [InlineData("APP.config", "App.Debug.config")]
+        [InlineData("App.Test.config", "App.Test.Debug.config")]
+        public void IsFileTransfromWithValidArguments(string docName, string trnName)
         {
-            Assert.True(PackageUtilities.IsFileTransform("App.config", "App.Debug.config", baseTestProjectConfigs));
-            Assert.True(PackageUtilities.IsFileTransform("App.config", "app.release.config", baseTestProjectConfigs));
-            Assert.True(PackageUtilities.IsFileTransform("APP.config", "App.Debug.config", baseTestProjectConfigs));
-            Assert.True(PackageUtilities.IsFileTransform("App.Test.config", "App.Test.Debug.config", baseTestProjectConfigs));
+            Assert.True(PackageUtilities.IsFileTransform(docName, trnName, baseTestProjectConfigs));
         }
 
         /// <summary>
         /// Tests <see cref="PackageUtilities.IsFileTransform(string, string, IEnumerable{string})"/> with invalid arguments
         /// </summary>
-        [Fact]
-        public void IsFileTransfromWithInvalidArguments()
+        [Theory]
+        [InlineData("App.config", "App.Test.Debug.config")]
+        [InlineData("App.Debug.config", "App.Debug.config")]
+        [InlineData("App.Debug.config", "App.Release.config")]
+        public void IsFileTransfromWithInvalidArguments(string docName, string trnName)
         {
-            Assert.False(PackageUtilities.IsFileTransform("App.config", "App.Test.Debug.config", baseTestProjectConfigs));
-            Assert.False(PackageUtilities.IsFileTransform("App.Debug.config", "App.Debug.config", baseTestProjectConfigs));
-            Assert.False(PackageUtilities.IsFileTransform("App.Debug.config", "App.Release.config", baseTestProjectConfigs));
+            Assert.False(PackageUtilities.IsFileTransform(docName, trnName, baseTestProjectConfigs));
         }
 
         /// <summary>
         /// Tests <see cref="PackageUtilities.IsFileTransform(string, string, IEnumerable{string})"/> with project configurations containing dots 
-        /// and file names with similar structures
+        /// and file names with similar structures. Tests valid names
         /// </summary>
-        [Fact]
-        public void IsFileTransformWithDottedConfigs()
+        [Theory]
+        [InlineData("App.config", "App.Debug.Test.config")]
+        [InlineData("App.System.config", "App.System.Debug.Test.config")]
+        [InlineData("App.config", "App.Test.Release.config")]
+        [InlineData("App.Test.config", "App.Test.Release.config")]
+        [InlineData("App.Test.config", "App.Test.Test.Release.config")]
+        [InlineData("App.config", "App.Test.Rel.config")]
+        public void IsFileTransformWithDottedConfigsAndValidNames(string docName, string trnName)
         {
-            IEnumerable<string> testProjectConfigsWithDots = new List<string>(new string[] { "Debug", "Debug.Test", "Release", "Test.Release", "Test.Rel"});
+            Assert.True(PackageUtilities.IsFileTransform(docName, trnName, testProjectConfigsWithDots));
+        }
 
-            Assert.True(PackageUtilities.IsFileTransform("App.config", "App.Debug.Test.config", testProjectConfigsWithDots));
-            Assert.True(PackageUtilities.IsFileTransform("App.System.config", "App.System.Debug.Test.config", testProjectConfigsWithDots));
-            Assert.True(PackageUtilities.IsFileTransform("App.config", "App.Test.Release.config", testProjectConfigsWithDots));
-            Assert.True(PackageUtilities.IsFileTransform("App.Test.config", "App.Test.Release.config", testProjectConfigsWithDots));
-            Assert.True(PackageUtilities.IsFileTransform("App.Test.config", "App.Test.Test.Release.config", testProjectConfigsWithDots));
-            Assert.True(PackageUtilities.IsFileTransform("App.config", "App.Test.Rel.config", testProjectConfigsWithDots));
-
-            Assert.False(PackageUtilities.IsFileTransform("App.config", "App.Release.Test.config", testProjectConfigsWithDots));
-            Assert.False(PackageUtilities.IsFileTransform("App.config", "App.Rel.Test.config", testProjectConfigsWithDots));
-            Assert.False(PackageUtilities.IsFileTransform("App.Test.config", "App.Test.Rel.config", testProjectConfigsWithDots));
-            Assert.False(PackageUtilities.IsFileTransform("App.Test.config", "App.Test.Test.config", testProjectConfigsWithDots));
-            Assert.False(PackageUtilities.IsFileTransform("App.Test.config", "App.Debug.Test.config", testProjectConfigsWithDots));
-            Assert.False(PackageUtilities.IsFileTransform("App.config", "Test.Rel.config", testProjectConfigsWithDots));
-            Assert.False(PackageUtilities.IsFileTransform("App.Test.Rel.config", "App.Test.Rel.config", testProjectConfigsWithDots));
+        /// <summary>
+        /// Tests <see cref="PackageUtilities.IsFileTransform(string, string, IEnumerable{string})"/> with project configurations containing dots 
+        /// and file names with similar structures. Tests invalid names
+        /// </summary>
+        [Theory]
+        [InlineData("App.config", "App.Release.Test.config")]
+        [InlineData("App.config", "App.Rel.Test.config")]
+        [InlineData("App.Test.config", "App.Test.Rel.config")]
+        [InlineData("App.Test.config", "App.Test.Test.config")]
+        [InlineData("App.Test.config", "App.Debug.Test.config")]
+        [InlineData("App.config", "Test.Rel.config")]
+        [InlineData("App.Test.Rel.config", "App.Test.Rel.config")]
+        public void IsFileTransformWithDottedConfigsAndInvalidNames(string docName, string trnName)
+        {
+            Assert.False(PackageUtilities.IsFileTransform(docName, trnName, testProjectConfigsWithDots));
         }
     }
 }
