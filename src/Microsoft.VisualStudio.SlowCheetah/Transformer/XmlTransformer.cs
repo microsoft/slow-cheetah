b6bc66340e71f40b6df267bcfb18aa632d9cc365
Jacob Viau <javia@microsoft.com>
2017-04-24 15:41:23 -0700

    SlowCheetah -> Microsoft.VisualStudio.SlowCheetah

---
+++ src/Microsoft.VisualStudio.SlowCheetah/Transformer/XmlTransformer.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.VisualStudio.SlowCheetah
+{
+    using System;
+    using System.Diagnostics.Contracts;
+    using System.IO;
+    using Microsoft.Web.XmlTransform;
+
+    /// <summary>
+    /// Transforms XML files utilizing Microsoft Web XmlTransform library
+    /// </summary>
+    public class XmlTransformer : ITransformer
+    {
+        private IXmlTransformationLogger logger = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlTransformer"/> class.
+        /// </summary>
+        public XmlTransformer()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlTransformer"/> class with an external logger.
+        /// </summary>
+        /// <param name="logger">External logger. Passed into the transformation</param>
+        /// <param name="useSections">Wheter or not to use sections while logging</param>
+        public XmlTransformer(ITransformationLogger logger, bool useSections)
+        {
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
+            this.logger = new XmlShimLogger(logger, useSections);
+        }
+
+        /// <inheritdoc/>
+        public bool Transform(string source, string transform, string destination)
+        {
+            // Parameter validation
+            Contract.Requires(!string.IsNullOrWhiteSpace(source));
+            Contract.Requires(!string.IsNullOrWhiteSpace(transform));
+            Contract.Requires(!string.IsNullOrWhiteSpace(destination));
+
+            // File validation
+            if (!File.Exists(source))
+            {
+                throw new FileNotFoundException("File to transform not found", source);
+            }
+
+            if (!File.Exists(transform))
+            {
+                throw new FileNotFoundException("Transform file not found", transform);
+            }
+
+            using (XmlTransformableDocument document = new XmlTransformableDocument())
+            using (XmlTransformation transformation = new XmlTransformation(transform, this.logger))
+            {
+                document.PreserveWhitespace = true;
+                document.Load(source);
+
+                var success = transformation.Apply(document);
+                if (success)
+                {
+                    document.Save(destination);
+                }
+
+                return success;
+            }
+        }
+    }
+}
