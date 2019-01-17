587551a8a90c89a89e373c124530b526b5a97914
Davi Lima Paulino <dalimapa@microsoft.com>
2019-01-16 16:13:18 -0800

    Async solution events Switch to UI thread to avoid deadlocks

--- src/Microsoft.VisualStudio.SlowCheetah.VS/Package/PackageSolutionEvents.cs
+++ src/Microsoft.VisualStudio.SlowCheetah.VS/Package/PackageSolutionEvents.cs
@@ -17,22 +17,17 @@ namespace Microsoft.VisualStudio.SlowCheetah.VS
         /// <summary>
         /// Initializes a new instance of the <see cref="PackageSolutionEvents"/> class.
         /// </summary>
-        /// <param name="serviceProvider">The VSPackage</param>
+        /// <param name="asyncPackage">The VSPackage</param>
         /// <param name="errorListProvider">The error list provider</param>
-        public PackageSolutionEvents(IServiceProvider serviceProvider, ErrorListProvider errorListProvider)
+        public PackageSolutionEvents(AsyncPackage asyncPackage, ErrorListProvider errorListProvider)
         {
-            this.ServiceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            this.Package = asyncPackage ?? throw new ArgumentNullException(nameof(asyncPackage));
             this.ErrorListProvider = errorListProvider ?? throw new ArgumentNullException(nameof(errorListProvider));
-
-            if (this.ServiceProvider.GetService(typeof(SVsSolutionBuildManager)) is IVsSolutionBuildManager solutionBuildManager)
-            {
-                solutionBuildManager.AdviseUpdateSolutionEvents(this, out this.solutionUpdateCookie);
-            }
         }
 
         private ErrorListProvider ErrorListProvider { get; }
 
-        private IServiceProvider ServiceProvider { get; }
+        private AsyncPackage Package { get; }
 
         /// <inheritdoc/>
         public int UpdateSolution_Begin(ref int pfCancelUpdate)
@@ -66,12 +61,34 @@ namespace Microsoft.VisualStudio.SlowCheetah.VS
             return VSConstants.S_OK;
         }
 
+        /// <summary>
+        /// Asynchronously registers the solution eve
+        /// </summary>
+        /// <returns>Async task</returns>
+        public async System.Threading.Tasks.Task RegisterEventsAsync()
+        {
+            await this.Package.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            if (await this.Package.GetServiceAsync(typeof(SVsSolutionBuildManager)) is IVsSolutionBuildManager solutionBuildManager)
+            {
+                solutionBuildManager.AdviseUpdateSolutionEvents(this, out this.solutionUpdateCookie);
+            }
+        }
+
         /// <inheritdoc/>
         public void Dispose()
         {
-            if (this.solutionUpdateCookie > 0 && this.ServiceProvider.GetService(typeof(SVsSolutionBuildManager)) is IVsSolutionBuildManager solutionBuildManager)
+            if (this.solutionUpdateCookie > 0)
             {
-                solutionBuildManager.UnadviseUpdateSolutionEvents(this.solutionUpdateCookie);
+                this.Package.JoinableTaskFactory.Run(async () =>
+                {
+                    await this.Package.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                    if (await this.Package.GetServiceAsync(typeof(SVsSolutionBuildManager)) is IVsSolutionBuildManager solutionBuildManager)
+                    {
+                        solutionBuildManager.UnadviseUpdateSolutionEvents(this.solutionUpdateCookie);
+                    }
+                });
             }
         }
     }
